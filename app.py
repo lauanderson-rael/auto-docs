e1ffa6a6ffe6734d10c9b50943ca94255da04e8a
lau-ifma <lauandersoncosta@acad.ifma.edu.br>
2025-01-30 10:54:47 -0300

    update campos

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from fillpdf import fillpdfs
 campos = list(fillpdfs.get_form_fields('edit2.pdf').keys())
 
 print(campos)
+
+nome = "Lauanderson Rael"
+cpf = "758.874.112.45"
+rg = '8373566232003-0'
+rua_numero = "Rua X, 45"
+bairro = "Anil"
+
+preenchimento = {
+    
+}
